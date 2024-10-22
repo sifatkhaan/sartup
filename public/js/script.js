43e5b7b978fac6f7019708af5ff5865897128f3b
sifatkhaan <sifatkhan9330@gmail.com>
2024-10-22 12:12:32 +0600

    drawer added

--- public/js/script.js
+++ public/js/script.js
@@ -186,3 +186,8 @@ closeMenuButton.addEventListener('click', () => {
     }
 });
 
+window.addEventListener('load', () => {
+    if (!window.location.hash) {
+        window.scrollTo(0, 0);
+    }
+});
